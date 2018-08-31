f20dafe5c041ec724ea0130f2d9590f64876a424
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-08-30 21:02:26 -0700

    completed main page forms

--- models/names.js
+++ models/names.js
@@ -1,18 +1,4 @@
 module.exports = function(sequelize, DataTypes) {
-<<<<<<< HEAD
-  let Names = sequelize.define("Names", {
-    name:DataTypes.STRING,
-
-    year:DataTypes.INTEGER,
-
-    gender:DataTypes.STRING,
-    
-    count:DataTypes.INTEGER
-   
-  });
-  return Names;
-};
-=======
   var Names = sequelize.define("Names", {
     name: DataTypes.STRING,
     year: DataTypes.INTEGER,
@@ -24,4 +10,3 @@ module.exports = function(sequelize, DataTypes) {
   }); 
   return Names;
 };
->>>>>>> master
