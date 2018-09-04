d37e22d9504c08bad768f9829239afd983a97cd2
Ericka Vanessa Hernandez <erickavanessahernandez@Erickas-MacBook-Air-2.local>
2018-09-04 16:42:25 -0700

    added option2 functionality

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
-    "username": process.env.DEV_DB_USER,
-    "password": process.env.DEV_DB_PASSWORD,
+    "username": "root",
+    "password": "Erivanher2017!",
     "database": "namesdb",
     "host": "localhost",
     "dialect": "mysql"
