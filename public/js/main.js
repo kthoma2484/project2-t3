9b29df26902e166fe65bbe487ee438d1978a2c31
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-09-04 23:04:40 -0700

    fixed front end name request code

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-
 //-----------------------Kim's Code--------------------------------
 
 $(function () {
@@ -98,49 +97,78 @@ $(function () {
 
 
     //Craig utilities
-    $('#starts-with').on('change', function(){
-      console.log("Name1 Value:", $('#name1').val())
+    $('#starts-with').on('change', function () {
+        console.log("Name1 Value:", $('#name1').val())
     })
-    
-    $('#opt1-btn').on('click', function(){
-      let numNames = $('#inputGroupSelect01').val();
-      console.log("numNames: ", numNames);
-      let firstLetter = $('#name1').val();
-      console.log("firstLetterVal:", firstLetter);
-      let gender = $('#inputGroupSelect02').val();
-      console.log("GenderVal:", gender); 
-      let lastName = $('#last-name').val();
-      console.log("lastName:", lastName);
-    
-      // Do an api request depending on conditions above
-      if(gender == 1){ //girl
-        $.get(`/api/wild/${firstLetter}/f`).then(function(data){
-          console.log(data);
-          alert("Data: " + data + "\nStatus: " + status);
-        });
-      } else if(gender == 2){ //boy
-        $.get(`/api/wild/${firstLetter}/m`).then(function(data){
-          console.log(data);
-          alert("Data: " + data + "\nStatus: " + status);
-        });
-      } else { //all
-        $.get(`/api/wild/${firstLetter}/`).then(function(data){
-          console.log(data);
-          alert("Data: " + data + "\nStatus: " + status);
-      });
-      }
-      
-     
 
-    function renderModal(data){
+    $('#opt1-btn').on('click', function () {
 
-    }
-  
-  
-  });
+        let numNames = $('#inputGroupSelect01').val();
+        console.log("numNames: ", numNames);
+        let firstLetter = $('#name1 option:selected').val();
+        console.log("firstLetterVal:", firstLetter);
+        let gender = $('#inputGroupSelect02').val();
+        console.log("GenderVal:", gender);
+        let lastName = $('#last-name').val();
+        console.log("lastName:", lastName);
+        let fullName = '';
+
+        if (firstLetter == 'Please pick a letter.') {
+            alert('Please choose the letter you wish the first name to begin with.');
+            return null;
+        } else if (gender == 'What gender name?') {
+            alert('Please select a gender');
+            return null;
+        } else if (lastName == null) {
+            alert('Please enter a last name');
+            return null;
+        } else if (!lastName.match('^[a-zA-Z]{3,16}$')) {
+            alert('Last name must only contain numbers and be 3-16 characters long. Please re-enter valid last name.');
+            return null;
+        }
+
+        // Do an api request depending on conditions above
+        if (gender == 1) { //girl
+            console.log('gender search for female')
+
+            $.get(`/api/wild/${firstLetter}/f`).then(function (data) {
+                console.log(data);
+                alert("Data: " + data + "\nStatus: " + status);
+            });
+        } else if (gender == 2) { //boy
+
+            console.log('gender search for male')
+            $.get(`/api/wild/${firstLetter}/m`).then(function (data) {
+                console.log(data);
+                alert("Data: " + data + "\nStatus: " + status);
+            });
+        } else { //all
+            console.log("gender search for either")
+
+            $.get(`/api/wild/${firstLetter}/`).then(function (data) {
+                //console.log(JSON.stringify(data))
+
+                //alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+
+                randomFirstName = data[0].name;
+                //console.log('this randomeFirstName: ' + randomFirstName)
+
+                fullName = randomFirstName + ' ' + lastName;
+                //console.log('this is fullName: ' + fullName) 
+
+                console.log('Your random name is: ' + fullName + '!')
+            });
+        }
+
+        function renderModal(data) {
+
+        }
 
 
- // If Origin name search is selected...
+    });
+
+
+    // If Origin name search is selected...
     $("#inputGroupSelect04").change(function () {
 
         // Shows search options for random name search
@@ -171,5 +199,3 @@ $(function () {
 
 
 });
-
-
